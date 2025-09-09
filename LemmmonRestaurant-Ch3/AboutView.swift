e4481f0985f23db5ec8b1bbe1393dfb84425007d
nrasaily <nareshsathi79@gmail.com>
2025-09-08 20:03:45 -0700

    MyFirstcommit

---
+++ LemmmonRestaurant-Ch3/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  LemmmonRestaurant-Ch3
+//
+//  Created by Nar Rasaily on 9/8/25.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
---
+++ Practice/Variables.swift
@@ -0,0 +1,8 @@
+//
+//  Variables.swift
+//  LemmmonRestaurant-Ch3
+//
+//  Created by Nar Rasaily on 9/8/25.
+//
+
+import Foundation
