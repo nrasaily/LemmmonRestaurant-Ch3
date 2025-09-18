18608549fbd1daf2ea638fab83fc79b29c1fa639
nrasaily <nareshsathi79@gmail.com>
2025-09-18 03:12:00 -0700

    MenuView is made

---
+++ LemmmonRestaurant-Ch3/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  LemmmonRestaurant-Ch3
+//
+//  Created by Nar Rasaily on 9/17/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
