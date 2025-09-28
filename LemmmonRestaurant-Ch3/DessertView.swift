02368956672f86f522cf2ed97e046a47db2b8219
nrasaily <nareshsathi79@gmail.com>
2025-09-28 12:03:47 -0700

    Assignment one completed for 102

---
+++ LemmmonRestaurant-Ch3/DessertView.swift
@@ -0,0 +1,18 @@
+//
+//  DessertView.swift
+//  LemmmonRestaurant-Ch3
+//
+//  Created by Nar Rasaily on 9/28/25.
+//
+
+import SwiftUI
+
+struct DessertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DessertView()
+}
