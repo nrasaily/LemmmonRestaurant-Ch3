b7f347cd90284417f888345263708c02955681e8
nrasaily <nareshsathi79@gmail.com>
2025-09-28 22:44:06 -0700

    working on ass 2

---
+++ LemmmonRestaurant-Ch3/MenuItemView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemView.swift
+//  LemmmonRestaurant-Ch3
+//
+//  Created by Nar Rasaily on 9/28/25.
+//
+
+import SwiftUI
+
+struct MenuItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemView()
+}
---
+++ LemmmonRestaurant-Ch3/Models/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  LemmmonRestaurant-Ch3
+//
+//  Created by Nar Rasaily on 9/28/25.
+//
+
+import Foundation
